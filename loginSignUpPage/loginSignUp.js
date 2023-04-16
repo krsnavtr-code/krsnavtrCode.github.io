d6ac25c47f016fb7aa4d5f3021d42dcd4583825e
Krishna <krsnavtr@gmail.com>
2023-04-16 17:05:11 +0530

    first commit

---
+++ loginSignUpPage/loginSignUp.js
@@ -0,0 +1,16 @@
+// Show Hode Password and Change Icon
+class Password {
+    constructor(passwordDiv) {
+      this.passwordDiv = document.querySelector(passwordDiv)
+      this.passwordField = document.querySelector(`#showHidePw`)
+      this.toggle = document.querySelector(`#toggleIcon`)
+      this.toggle.addEventListener("click", this.showHidePw.bind(this, this.passwordField, this.toggle))
+    }
+    showHide(passwordField, toggle) {
+      if(passwordField.type == "password") {
+        console.log(`password`)
+      } else {
+        console.log(`text`)
+      }
+    }
+  }
