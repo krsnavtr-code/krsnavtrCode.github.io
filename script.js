011dacfae1f843b9abdb877e8f3c9e770d4cb2d4
Krishna <krsnavtr@gmail.com>
2023-04-20 10:42:26 +0530

    time two content

--- script.js
+++ script.js
@@ -27,158 +27,79 @@ function includeHTML() {
 }
 includeHTML();
 
-// Clon blog Item
-let cloneBlog = document.querySelector(`#clonBlogElm`);
-let changeContent = [
-  {
-    imgCoverPath: "./images/bg3.jpg",
-    imgAlt: "",
-    blogDate: "2015-",
-    blogTitle: "qwertyjk",
-    blogContent:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, suscipit?",
-    buttonUrl: "item-content-button",
-  },
-  {
-    imgCoverPath: "./images/bg4.jpg",
-    imgAlt: "",
-    blogDate: "2015-",
-    blogTitle: "qwertyjk",
-    blogContent:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, suscipit?",
-    buttonUrl: "item-content-button",
-  },
-  {
-    imgCoverPath: "./images/bg1.jpg",
-    imgAlt: "",
-    blogDate: "2015-",
-    blogTitle: "qwertyjk",
-    blogContent:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, suscipit?",
-    buttonUrl: "item-content-button",
-  },
-  {
-    imgCoverPath: "./images/bg4.jpg",
-    imgAlt: "",
-    blogDate: "2015-",
-    blogTitle: "qwertyjk",
-    blogContent:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, suscipit?",
-    buttonUrl: "item-content-button",
-  },
-];
-function repeat(
-  i,
-  imgCoverPath,
-  imgAlt,
-  blogDate,
-  blogTitle,
-  blogContent,
-  buttonUrl
-) {
-  return `
-  <div class="col-lg-6" id="${i}">
-      <div class="card mb-4 "><a href="#!"><img class="card-img-top"
-                  src="${imgCoverPath}" alt="${imgAlt}"></a>
-          <div class="card-body">
-              <div class="small text-muted">${blogDate}</div>
-              <h2 class="card-title h4">${blogTitle}</h2>
-              <p class="card-text">${blogContent}</p>
-              <a class="btn btn-warning btn-sm" href="${buttonUrl}">Read more →</a>
-          </div>
-      </div>
-  </div>`;
-}
-changeContent.forEach((item, i) => {
-  cloneBlog.insertAdjacentHTML(
-    "beforeend",
-    repeat(
-      i,
-      item.imgCoverPath,
-      item.imgAlt,
-      item.blogDate,
-      item.blogTitle,
-      item.blogContent,
-      item.buttonUrl
-    )
-  );
-});
-
-
 // Clon Project Item
-
-let cloneProjectItem = document.querySelector(`#clonProjectElm`);
-let changeProjectContent = [
-  {
-    imgCoverPath: "./images/bg3.jpg",
-    imgAlt: "",
-    blogDate: "2015-",
-    blogTitle: "qwertyjk",
-    blogContent:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, suscipit?",
-    buttonUrl: "item-content-button",
-  },
-  {
-    imgCoverPath: "./images/bg4.jpg",
-    imgAlt: "",
-    blogDate: "2015-",
-    blogTitle: "qwertyjk",
-    blogContent:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, suscipit?",
-    buttonUrl: "item-content-button",
-  },
-  {
-    imgCoverPath: "./images/bg1.jpg",
-    imgAlt: "",
-    blogDate: "2015-",
-    blogTitle: "qwertyjk",
-    blogContent:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, suscipit?",
-    buttonUrl: "item-content-button",
-  },
-  {
-    imgCoverPath: "./images/bg4.jpg",
-    imgAlt: "",
-    blogDate: "2015-",
-    blogTitle: "qwertyjk",
-    blogContent:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, suscipit?",
-    buttonUrl: "item-content-button",
-  },
-];
-function repeat(
-  i,
-  imgCoverPath,
-  imgAlt,
-  blogDate,
-  blogTitle,
-  blogContent,
-  buttonUrl
-) {
-  return `
-  <div class="col-lg-6" id="${i}">
-      <div class="card mb-4 "><a href="#!"><img class="card-img-top"
-                  src="${imgCoverPath}" alt="${imgAlt}"></a>
-          <div class="card-body">
-              <div class="small text-muted">${blogDate}</div>
-              <h2 class="card-title h4">${blogTitle}</h2>
-              <p class="card-text">${blogContent}</p>
-              <a class="btn btn-warning btn-sm" href="${buttonUrl}">Read more →</a>
-          </div>
-      </div>
-  </div>`;
-}
-changeProjectContent.forEach((item, i) => {
-  cloneProjectItem.insertAdjacentHTML(
-    "beforeend",
-    repeat(
-      i,
-      item.imgCoverPath,
-      item.imgAlt,
-      item.blogDate,
-      item.blogTitle,
-      item.blogContent,
-      item.buttonUrl
-    )
-  );
-});
+// let cloneProjectItem = document.querySelector(`#clonProjectElm`);
+// let changeProjectContent = [
+//   {
+//     imgCoverPath: "./images/bg3.jpg",
+//     imgAlt: "",
+//     blogDate: "2015-",
+//     blogTitle: "qwertyjk",
+//     blogContent:
+//       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, suscipit?",
+//     buttonUrl: "item-content-button",
+//   },
+//   {
+//     imgCoverPath: "./images/bg4.jpg",
+//     imgAlt: "",
+//     blogDate: "2015-",
+//     blogTitle: "qwertyjk",
+//     blogContent:
+//       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, suscipit?",
+//     buttonUrl: "item-content-button",
+//   },
+//   {
+//     imgCoverPath: "./images/bg1.jpg",
+//     imgAlt: "",
+//     blogDate: "2015-",
+//     blogTitle: "qwertyjk",
+//     blogContent:
+//       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, suscipit?",
+//     buttonUrl: "item-content-button",
+//   },
+//   {
+//     imgCoverPath: "./images/bg4.jpg",
+//     imgAlt: "",
+//     blogDate: "2015-",
+//     blogTitle: "qwertyjk",
+//     blogContent:
+//       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, suscipit?",
+//     buttonUrl: "item-content-button",
+//   },
+// ];
+// function repeat(
+//   i,
+//   imgCoverPath,
+//   imgAlt,
+//   blogDate,
+//   blogTitle,
+//   blogContent,
+//   buttonUrl
+// ) {
+//   return `
+//   <div class="col-lg-6" id="${i}">
+//       <div class="card mb-4 "><a href="#!"><img class="card-img-top"
+//                   src="${imgCoverPath}" alt="${imgAlt}"></a>
+//           <div class="card-body">
+//               <div class="small text-muted">${blogDate}</div>
+//               <h2 class="card-title h4">${blogTitle}</h2>
+//               <p class="card-text">${blogContent}</p>
+//               <a class="btn btn-warning btn-sm" href="${buttonUrl}">Read more →</a>
+//           </div>
+//       </div>
+//   </div>`;
+// }
+// changeProjectContent.forEach((item, i) => {
+//   cloneProjectItem.insertAdjacentHTML(
+//     "beforeend",
+//     repeat(
+//       i,
+//       item.imgCoverPath,
+//       item.imgAlt,
+//       item.blogDate,
+//       item.blogTitle,
+//       item.blogContent,
+//       item.buttonUrl
+//     )
+//   );
+// });
